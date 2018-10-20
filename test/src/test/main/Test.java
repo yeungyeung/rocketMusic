d9a2319d0a2dea5c7241d5831f8b40a29420764b
yeangyeang <1819196680@qq.com>
2018-10-20 23:34:52 +0800

    第一次提交代码

---
+++ test/src/test/main/Test.java
@@ -0,0 +1,13 @@
+package test.main;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0;
+		int j = i+++ ++i;
+		System.out.println(j+"    "+i);
+	}
+
+}
